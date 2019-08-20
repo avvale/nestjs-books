3bc4871e8dfd96d711c34b66009dd4a041278d00
Carlos Rodríguez Palacín <carlos.rodriguez@techedgegroup.es>
2019-08-20 16:50:47 +0200

    database config in external file

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,16 +12,7 @@ import { Reader } from './reader/entities/reader.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            "type": "mysql",
-            "host": "localhost",
-            "port": 33001,
-            "username": "root",
-            "password": "123456",
-            "database": "book_local",
-            "entities": [__dirname + '/**/*.entity{.ts,.js}'],
-            "synchronize": true
-        }),
+        TypeOrmModule.forRoot(),
         TypeOrmModule.forFeature([
             Author, 
             Book, 
